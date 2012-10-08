d622ef3e1d9f1cbdf90c30a7b0d6755b45fcf8de
Michael Biven <michael.biven@gmail.com>
2012-10-08 12:07:25 -0700

    Revert "allow for name_suffix to be set if host_prefix attribute is present"
    This reverts commit 97c529737ff8c0ccace89e3e42d24bb47d581430.

--- lib/capistrano/chef.rb
+++ lib/capistrano/chef.rb
@@ -6,7 +6,7 @@ require 'chef/search/query'
 module Capistrano::Chef
   # Set up chef configuration
   def self.configure_chef
-    knife        = Chef::Knife.new
+    knife = Chef::Knife.new
     # If you don't do this it gets thrown into debug mode
     knife.config = { :verbosity => 1 }
     knife.configure_chef
@@ -14,8 +14,23 @@ module Capistrano::Chef
 
   # Do a search on the Chef server and return an attary of the requested
   # matching attributes
-  def self.search_chef_nodes(query = '*:*')
-    Chef::Search::Query.new.search(:node, query)[0]
+  def self.search_chef_nodes(query = '*:*', arg = :ipaddress)
+    search_proc = \
+      case arg
+      when Proc
+        arg
+      when Hash
+        iface, family = arg.keys.first.to_s, arg.values.first.to_s
+        Proc.new do |n|
+          addresses = n["network"]["interfaces"][iface]["addresses"]
+          addresses.select{|address, data| data["family"] == family }.keys.first
+        end
+      when Symbol, String
+        Proc.new{|n| n[arg.to_s]}
+      else
+        raise ArgumentError, 'Search arguments must be Proc, Hash, Symbol, String.'
+      end
+    Chef::Search::Query.new.search(:node, query)[0].map(&search_proc)
   end
 
   def self.get_data_bag_item(id, data_bag = :apps)
@@ -27,13 +42,8 @@ module Capistrano::Chef
     self.configure_chef
     configuration.set :capistrano_chef, self
     configuration.load do
-      def chef_role(name, query = '*:*', options = { })
-        capistrano_chef.search_chef_nodes(query).each do |server|
-          attribute = options.delete(:attribute)
-          options.merge!(name_suffix: server.name.split("#{host_prefix}-").last) if exists? :host_prefix
-          role name, server[attribute], options
-        end
-
+      def chef_role(name, query = '*:*', options = {})
+        role name, *(capistrano_chef.search_chef_nodes(query, options.delete(:attribute)) + [options])
       end
 
       def set_from_data_bag(data_bag = :apps)
